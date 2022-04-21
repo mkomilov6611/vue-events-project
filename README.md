28b10b3ef5374a4c80db16f6d6b919be36393d15
Mukhammadnazar Komilov <mkomilov6611@gmail.com>
2022-04-21 13:15:28 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+Here are the commands you’ll want handy once you have it installed. I’m assuming you already have npm installed.
+
+To install dependencies:
+
+```
+    $ npm install
+```
+
+To run the server:
+
+```
+    $ npm run serve
+```
+
+To get json-server, which we use for our mock database (Starting on API calls with Axios)
+
+```
+    $ npm install -g json-server
+```
+
+To run the mock database
+
+```
+    $ json-server --watch db.json
+```
---
+++ src/views/Example.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <input
+      type="email"
+      v-model="email"
+      placeholder="What`s your email"
+      @blur="$v.email.$touch()"
+    />
+    <div v-if="$v.email.$error">
+      <p v-if="!$v.email.email">Please enter a valid email</p>
+      <p v-if="!$v.email.required">Email is required</p>
+    </div>
+
+    <button type="submit">Submit</button>
+  </div>
+</template>
+
+<script>
+import { required, email } from 'vuelidate/lib/validators'
+
+export default {
+  data() {
+    return {
+      email: null,
+    }
+  },
+  validations: {
+    email: {
+      required,
+      email,
+    },
+  },
+}
+</script>
+
+<style>
+</style>
